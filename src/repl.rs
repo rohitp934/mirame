7f2188fa9303f91b5ff50267e0e7395ded0ac642
Rohit Prakash <rohitp934@gmail.com>
2024-02-06 20:40:31 -0500

    repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,16 +1,37 @@
-use mirame::{lexer::Lexer, token::Token};
-use std::io::{self, Write};
+use mirame::{eval::eval, lexer::Lexer, object::env::Env, parser::Parser, token::Token};
+use std::{
+    cell::RefCell,
+    io::{self, Write},
+    rc::Rc,
+};
 
 pub const PROMPT: &str = "> ";
 
 pub fn start(istream: std::io::Stdin) {
+    let user = whoami::username();
+
+    let env = Rc::new(RefCell::new(Env::new()));
+
     istream.lines().for_each(|line| {
         if let Ok(line) = line {
-            let mut lex = Lexer::new(line);
-            let mut token = lex.next_token();
-            while token != Token::Eof {
-                println!("{}", token);
-                token = lex.next_token();
+            let mut parser = Parser::new(Lexer::new(line));
+
+            let program = parser.parse_program();
+            if !parser.errors().is_empty() {
+                println!(" parser errors:");
+                for msg in parser.errors() {
+                    println!("\t{:?}", msg);
+                }
+                return;
+            }
+
+            match eval(&program, Rc::clone(&env)) {
+                Ok(obj) => {
+                    println!("{}", obj);
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
             }
             print!("{} ", PROMPT);
             let _ = io::stdout().flush();
